126fee902736964aaa4472e1466a43b20182c642
Kevin Lin <github@kevinlinp.org>
2025-04-20 11:54:46 -0700

    tweak deploy script

--- scripts/deploy.zsh
+++ scripts/deploy.zsh
@@ -6,4 +6,4 @@ ncc build youtube-transcript.js -m --target es2024
 
 zip -j deploy/youtube-transcript.zip dist/*
 
-scw function deploy name=youtube-transcript namespace-id=2dfd141b-d6fd-4035-8db0-a83e32bba2bd zip-file=deploy/youtube-transcript.zip runtime=node22
+scw function deploy name=youtube-transcript region=nl-ams namespace-id=2dfd141b-d6fd-4035-8db0-a83e32bba2bd zip-file=deploy/youtube-transcript.zip runtime=node22
